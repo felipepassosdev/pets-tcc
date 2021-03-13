cfaf5736a4ba985991f3d5535d83f0aeba6e9d4c
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2021-03-13 14:49:28 -0300

    start project

---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,16 @@
+import { Card, Container } from '@material-ui/core';
+import React from 'react';
+
+// import { Container } from './styles';
+
+const CardComponent: React.FC = () => {
+  return (
+      <>
+        <Card>
+          <h1>hello world</h1>
+        </Card>
+      </>
+  );
+}
+
+export default CardComponent;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { Card, Container } from '@material-ui/core';
+import React from 'react';
+import CardComponent from '../components/CardComponent';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+      <Container>
+          <CardComponent>
+              
+          </CardComponent>
+      </Container>
+  );
+}
+
+export default Home;
