35bff25296074db9df5b35ac211546bee92264ae
felipepassosdev <felipepassosc@gmail.com>
2021-03-30 22:04:37 -0300

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Home from './pages/Home';
 import Login from './pages/user/login';
 import Register from './pages/user/register';
 import PetRegister from './pages/pet/register';
+import Menu from './components/Menu';
 const App: React.FC = () => {
   return (
     <>
+      <Menu/>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} />
