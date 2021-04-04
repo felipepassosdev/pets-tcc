fc9c8258a5f5d0a478b0534d5aac47d898a283fb
David Silva <davidlsilva.sp@gmail.com>
2021-04-04 18:22:25 -0300

    useState e correções nos forms de login e registro

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,83 +1,75 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import React, { useState } from "react";
 import {
   Button,
-  Checkbox,
   Container,
-  InputLabel,
   TextField,
   Box,
+  Grid,
+  Typography,
 } from "@material-ui/core";
 import { useHistory } from "react-router";
 
-const useStyles = makeStyles({
-  formFlex: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  LembrarDeMim: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  FormSpacing: {
-    marginBottom: "1rem",
-  },
-});
-
-const Login: React.FC = () => {
+function Login() {
   let history = useHistory();
-  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <Container>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={8}>
-          <h1>Bem-vindo de volta</h1>
-          <form className={classes.formFlex}>
+        <Grid item xs={12} sm={6}>
+          <Box mt={2}>
+            <Typography variant="h5">Bem-vindo de volta</Typography>
+          </Box>
+
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              alert(`Login DEBUG: email: "${email}", senha: "${password}"`);
+            }}
+          >
             <TextField
-              required
-              id="login_email"
-              label="E-mail"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              id="Email"
+              label="Digite seu email"
+              type="text"
               variant="outlined"
-              className={classes.FormSpacing}
-            />
-            <TextField
+              margin="normal"
               required
-              id="login_password"
-              label="Password"
+              fullWidth
+            />
+
+            <TextField
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              id="Password"
+              label="Digite sua senha"
               type="password"
               variant="outlined"
-              className={classes.formFlex}
+              margin="normal"
+              required
+              fullWidth
             />
-            <Box className={classes.LembrarDeMim}>
-              <Checkbox
-                value="login_rememberme"
-                inputProps={{ "aria-label": "Checkbox A" }}
-              />
-              <InputLabel htmlFor="login_rememberme">Lembrar Senha</InputLabel>
-            </Box>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                alert("clicado");
-              }}
-            >
-              Entrar
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Fazer Login
             </Button>
           </form>
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <Box m="2rem">
-            <h2>Novo por aqui?</h2>
-            <p>Precisamos de algumas informações suas para começarmos</p>
+        <Grid item xs={12} sm={6}>
+          <Box mt={2}>
+            <Typography variant="h5">Primeira vez aqui?</Typography>
+            <Typography variant="subtitle1">
+              Precisamos de algumas informações suas para começarmos
+            </Typography>
             <Button
               variant="outlined"
               color="primary"
               onClick={() => {
-                history.push('/register')
+                history.push("/register");
               }}
             >
               Criar Conta
@@ -87,6 +79,6 @@ const Login: React.FC = () => {
       </Grid>
     </Container>
   );
-};
+}
 
 export default Login;
--- src/pages/user/register.tsx
+++ src/pages/user/register.tsx
@@ -1,100 +1,109 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import {
   Button,
   Checkbox,
   Container,
-  InputLabel,
   TextField,
   Box,
+  FormControlLabel,
+  Typography,
 } from "@material-ui/core";
 import { useHistory } from "react-router";
 
-const useStyles = makeStyles({
-  formFlex: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  LembrarDeMim: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  FormSpacing: {
-    marginBottom: "1rem",
-  },
-});
-
-const Register: React.FC = () => {
+function Register() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [termos, setTermos] = useState(false);
   let history = useHistory();
-  const classes = useStyles();
   return (
     <Container>
       <Grid container spacing={3}>
-      <Grid item xs={12} sm={4}>
-          <Box m="2rem">
-            <h2>Já esteve aqui?</h2>
-            <p>Sentimos sua falta</p>
+        <Grid item xs={12} sm={6}>
+          <Box mt={2}>
+            <Typography variant="h5">Já esteve aqui?</Typography>
+            <Typography variant="subtitle1">Sentimos sua falta!</Typography>
             <Button
               variant="outlined"
               color="primary"
               onClick={() => {
-                history.push('/login')
+                history.push("/login");
               }}
             >
               Fazer Login
             </Button>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={8}>
-          <h1>Criando uma nova conta</h1>
-          <form className={classes.formFlex}>
-          <TextField
+        <Grid item xs={12} sm={6}>
+          <Box mt={2}>
+            <Typography variant="h5">Criando uma conta nova</Typography>
+          </Box>
+
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              alert(
+                `Cadastro DEBUG: email: "${email}", senha: "${password}", Nome: "${nome}",Temos Aceite "${termos}"`
+              );
+            }}
+          >
+            <TextField
               required
-              id="Register_Name"
-              label="Seu Nome"
+              id="Name"
+              label="Qual o seu Nome"
               variant="outlined"
-              className={classes.FormSpacing}
+              margin="normal"
+              fullWidth
+              value={nome}
+              onChange={(event) => {
+                setNome(event.target.value);
+              }}
             />
             <TextField
               required
-              id="Register_email"
-              label="E-mail"
+              id="Email"
+              label="Digite sua email"
               variant="outlined"
-              className={classes.FormSpacing}
+              margin="normal"
+              fullWidth
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
             />
             <TextField
               required
-              id="Register_password"
-              label="Password"
+              id="Password"
+              label="Digite sua senha"
               type="password"
               variant="outlined"
-              className={classes.formFlex}
-            />
-            <Box className={classes.LembrarDeMim}>
-              <Checkbox
-                value="Register_Terms"
-                inputProps={{ "aria-label": "Checkbox A" }}
-              />
-              <InputLabel htmlFor="Register_Terms">Li e aceito os termos e condições</InputLabel>
-            </Box>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                alert("clicado");
+              margin="normal"
+              fullWidth
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
               }}
-            >
-              Criar conta
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  value={termos}
+                  onChange={(event) => {
+                    setTermos(event.target.checked);
+                  }}
+                />
+              }
+              label="Aceito os termos de uso"
+            />
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Finalizar Cadastro
             </Button>
           </form>
         </Grid>
-
       </Grid>
     </Container>
   );
-};
+}
 
 export default Register;
