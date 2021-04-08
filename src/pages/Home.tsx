df524432e5a3e314f468e3f515c97fc80a4d2f0e
David Silva <davidlsilva.sp@gmail.com>
2021-04-07 21:34:26 -0300

    Base card pet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,65 @@
-import { Button, Card, Container } from '@material-ui/core';
-import React from 'react';
-import { useHistory } from 'react-router';
+import { Button, Card, Container, Grid } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
+import PetCard from "../components/PetCard";
+import api from "../services/api";
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
   let history = useHistory();
+  const [pets, setPets] = useState<Array<any>>();
+  async function getGames() {
+    api.get("pets").then((res) => {
+      setPets(res.data);
+      console.log(res.data);
+    });
+  }
+  useEffect(() => {
+    getGames();
+  }, []);
   return (
-      <Container>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={4}>
           <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => {
-                history.push('/login')
-              }}
-            >
-              Form Login
-            </Button>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => {
-                history.push('/login')
-              }}
-            >
-              Form Cadastro
-            </Button>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => {
-                history.push('/pet/register')
-              }}
-            >
-              Pet Register
-            </Button>
-      </Container>
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              history.push("/login");
+            }}
+          >
+            Form Login
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              history.push("/login");
+            }}
+          >
+            Form Cadastro
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              history.push("/pet/register");
+            }}
+          >
+            Pet Register
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          {pets &&
+            pets.map((pet) => {
+              return <PetCard petInfo={pet} />;
+            })}
+        </Grid>
+      </Grid>
+    </Container>
   );
-}
+};
 
 export default Home;
